e73b3824b7f4e397935648dc5305ab73aa576d51
Jarrod A. Verhagen <jarrod_av@hotmail.com>
2020-04-07 21:00:25 +0100

    Add marble piece

--- SoloNoble/components/PieceView.js
+++ SoloNoble/components/PieceView.js
@@ -1,6 +1,7 @@
 import React from "react";
-import { View, Text, StyleSheet, TouchableHighlight } from "react-native";
+import { View, StyleSheet, TouchableHighlight } from "react-native";
 import { TILE_SIZE } from "../constants";
+import Marble from '../svg/Marble';
 
 class PieceView extends React.Component {
 
@@ -19,7 +20,7 @@ class PieceView extends React.Component {
         var posStyle = {
             left: this.props.x*TILE_SIZE,
             top: this.props.y*TILE_SIZE,
-            borderColor: this.props.isSelected ? 'blue' : 'black',
+            // borderColor: this.props.isSelected ? 'blue' : 'black',
         };
         var style = [styles.tile,posStyle];
         // console.log('this.props.piece:',this.props.piece);
@@ -28,23 +29,42 @@ class PieceView extends React.Component {
             style.push(styles['color'+this.props.piece.value]);
         }
         return <TouchableHighlight style={style} onPress={this.onSelect}>
-                    <View/>
+                    <View>
+                        { this.props.isSelected ?
+                            <View style={styles.sel}/>
+                        : null }
+                        <Marble/>
+                    </View>
                 </TouchableHighlight>;
     }
 
 }
 
+const borderSelSize = 6;
+
 const styles = StyleSheet.create({
     tile: {
         position: 'absolute',
-        borderColor: 'black',
-        borderWidth: 10,
+        // borderColor: 'blue',
+        // borderWidth: 10,
         width: TILE_SIZE*0.8,
         height: TILE_SIZE*0.8,
         margin: TILE_SIZE*0.1,
         backgroundColor: 'white',
         borderRadius: TILE_SIZE,
 
+    },
+    sel: {
+        position: 'absolute',
+        borderColor: 'orange',
+        borderWidth: borderSelSize,
+        width: TILE_SIZE*0.8+borderSelSize*2,
+        height: TILE_SIZE*0.8+borderSelSize*2,
+        // margin: TILE_SIZE*0.1,
+        // backgroundColor: 'white',
+        borderRadius: TILE_SIZE,
+        top: -borderSelSize,
+        left: -borderSelSize,
     },
     color2: {
         backgroundColor: 'red',
